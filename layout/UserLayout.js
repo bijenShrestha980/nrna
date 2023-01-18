ab914e25b1e9cd82fda6113b5853b3a856cba0b4
bijenShrestha980 <s.bijen93@gmail.com>
2023-01-18 17:02:52 +0545

    dyspinner

--- layout/UserLayout.js
+++ layout/UserLayout.js
@@ -1,14 +1,15 @@
 import dynamic from "next/dynamic";
 import { memo } from "react";
+import Spinner from "./Loader/Spinner";
 // COMPONENTS
 const UserBreadcrumb = dynamic(() => import("./Breadcrumb/UserBreadcrumb"), {
-  loading: () => "Loading...",
+  loading: () => <Spinner />,
 });
 const Header = dynamic(() => import("./Header/Header"), {
-  loading: () => "Loading...",
+  loading: () => <Spinner />,
 });
 const UserHeader = dynamic(() => import("./Header/UserHeader"), {
-  loading: () => "Loading...",
+  loading: () => <Spinner />,
 });
 
 const UserLayout = ({ children }) => {
